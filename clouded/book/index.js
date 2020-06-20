712a27fb00f33ebfbedcaf93ada5514d102c725b
qhuang05 <qhuang05@sina.com>
2020-06-20 09:13:13 +0800

    update

--- clouded/book/index.js
+++ clouded/book/index.js
@@ -1,7 +1,7 @@
 const cloud = require('wx-server-sdk')
 const axios = require('axios')
 const doubanbook = require('doubanbook')
-const cherrio = require('cherrio')
+// const cherrio = require('cherrio')
 
 cloud.init()
 
@@ -9,7 +9,7 @@ async function getDoubanBook(isbn){
   const url = `https://search.douban.com/book/subject_search?search_text=${isbn}`;
   // 抓取网页数据
   let searchInfo = await axios.get(url);
-  // console.log('searchInfo', searchInfo);
+  console.log('searchInfo', searchInfo);
   let reg = /window\.__DATA__ = "(.*)"/;
   let searchData = '';
   if (reg.test(searchInfo.data)){
@@ -18,6 +18,7 @@ async function getDoubanBook(isbn){
   return searchData;
 }
 
+getDoubanBook('9787121276576');
 // 云函数入口函数
 exports.main = async (event, context) => {
   const { isbn } = event;
